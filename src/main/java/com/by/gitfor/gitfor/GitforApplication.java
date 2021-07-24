04adccec944d4acd5bbfa79a0be38d296b5260ae
acai <acai@byai.com>
2021-07-24 11:21:54 +0800

    first commit

---
+++ src/main/java/com/by/gitfor/gitfor/GitforApplication.java
@@ -0,0 +1,13 @@
+package com.by.gitfor.gitfor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitforApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitforApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/by/gitfor/gitfor/GitforApplicationTests.java
@@ -0,0 +1,13 @@
+package com.by.gitfor.gitfor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitforApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
